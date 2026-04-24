bf6363a393165be7e40e7ca212ca07ffc32f1f7e
Roger Taylor <rotay11@Rogers-MacBook-Pro.local>
2026-04-24 08:00:22 -0700

    Update APK to fix HomeScreen crash

--- src/pages/DownloadPage.jsx
+++ src/pages/DownloadPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const APK_URL = 'https://expo.dev/artifacts/eas/rx76abkBqRp26uMoiS5PXi.apk'
+const APK_URL = 'https://expo.dev/artifacts/eas/tzXJ9CTwDcWGwX1uTq91F2.apk'
 const IOS_URL = 'https://testflight.apple.com/join/h34Kc8M6'
 
 export default function DownloadPage() {

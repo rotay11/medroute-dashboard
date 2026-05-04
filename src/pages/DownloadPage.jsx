59137e3ccc11e1a5974846e2c8549d5bc5ed2d7a
Roger Taylor <rotay11@Rogers-MacBook-Pro.local>
2026-05-04 08:34:26 -0700

    Update APK with signature Save/Clear fix

--- src/pages/DownloadPage.jsx
+++ src/pages/DownloadPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const APK_URL = 'https://expo.dev/artifacts/eas/5unufwzy3Atbf2J94nnUP7.apk'
+const APK_URL = 'https://expo.dev/artifacts/eas/gQD9vzcEzCnNwFPxsFDe9X.apk'
 const IOS_URL = 'https://testflight.apple.com/join/h34Kc8M6'
 
 export default function DownloadPage() {

f1c2ab3e85d8cc0f270a9f480cd502d48e2a8b2d
Roger Taylor <rotay11@Rogers-MacBook-Pro.local>
2026-06-16 09:14:17 -0700

    Update APK URL with Erick feedback fixes

--- src/pages/DownloadPage.jsx
+++ src/pages/DownloadPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const APK_URL = 'https://expo.dev/artifacts/eas/jjejyKvHjrdKuHg6SPVzgw.apk'
+const APK_URL = 'https://expo.dev/artifacts/eas/EQNOjL_ywLtaNT4K3ZqK3gtfyPEqMw1SqQU4jWabwe4.apk'
 const IOS_URL = 'https://testflight.apple.com/join/h34Kc8M6'
 
 export default function DownloadPage() {
